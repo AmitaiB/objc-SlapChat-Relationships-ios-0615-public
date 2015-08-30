9ac74e0ba00f96fd93fde96995e3a1678eb2fbb6
AmitaiB <blickstein@gmail.com>
2015-08-30 17:47:08 -0400

    rewires the storyboard order with new recipientsTableVC

---
+++ slapChat/FISRecipientsTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  FISRecipientsTableViewController.h
+//  slapChat
+//
+//  Created by Amitai Blickstein on 8/30/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISDataStore.h"
+
+@interface FISRecipientsTableViewController : UITableViewController
+
+@property (nonatomic, strong) FISDataStore *sharedStore;
+@property (nonatomic, strong) NSArray *managedRecipientObjects;
+
+@end
